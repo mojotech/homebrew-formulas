a8dbe908b52bc8f3cccdd27f5fe50e4ccaef062e
Will Farrell <will@mojotech.com>
2015-12-15 14:24:52 -0500

    But really, fix an issue with module loading.

--- instantclient-basiclite.rb
+++ instantclient-basiclite.rb
@@ -1,5 +1,5 @@
 require 'formula'
-require File.expand_path('./util/instantclient', __FILE__)
+require File.expand_path('../util/instantclient', __FILE__)
 
 # A formula that installs the Instantclient Basiclite package.
 class InstantclientBasiclite < Formula
